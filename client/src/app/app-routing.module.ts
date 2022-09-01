8ee6f05e6f2c1811a4a7b632f7d709b6948e0e31
George-mFF4G <gm1362@fayoum.edu.eg>
2022-09-01 19:24:41 +0200

    routing-navigation

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,38 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
-const routes: Routes = [];
+// const routes: Routes = [];
+// @NgModule({
+//   imports: [RouterModule.forRoot(routes)],
+//   exports: [RouterModule]
+// })
+// export class AppRoutingModule { }
+
+
+const appRoutes: Routes=[
+  {path:'',
+component: HomeComponent
+},{path:'dashboard',
+component: DashboardComponent
+},{
+  path:'**',
+  component: HomeComponent
+}
+];
+
+
+
+
+
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  declarations: [],
+  imports: [RouterModule.forRoot(appRoutes)],
+  providers: [],
+  bootstrap: [],
+  exports:[RouterModule]
 })
 export class AppRoutingModule { }
+
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+// import { AppRoutingModule } from './app-routing.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HomeComponent } from './components/home/home.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    HomeComponent,
+    DashboardComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
+    // AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
